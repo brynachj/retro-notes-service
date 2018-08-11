5a21f02ae35b77c978ec9862b57cb5664c8eb339
Brynach Jones <brynach.jones@gmail.com>
2018-08-11 08:54:37 +0100

    Initial commit of basic springboot project with rest repositories and embedded mongoDB dependencies.

---
+++ src/main/java/com/brynachj/retronotesservice/RetroNotesServiceApplication.java
@@ -0,0 +1,12 @@
+package com.brynachj.retronotesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroNotesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroNotesServiceApplication.class, args);
+	}
+}
